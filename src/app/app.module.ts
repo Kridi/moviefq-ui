1e6ded0724c79d1c1c638583d8fd1fd7258ba8ac
0nida <piekute.rimante@gmail.com>
2017-02-28 21:29:38 +0200

    updates logo menu, started footer search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { AppComponent } from './app.component';
 
 import { LogoComponent } from './logo/logo.component';
 import { MenuComponent } from './menu/menu.component';
+import { SearchComponent } from './search/search.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
     LogoComponent,
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    SearchComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/search/search.module.ts
@@ -0,0 +1,20 @@
+// Angular Imports
+import { NgModule } from '@angular/core';
+
+// This Module's Components
+import { SearchComponent } from './search.component';
+
+@NgModule({
+    imports: [
+
+    ],
+    declarations: [
+        SearchComponent,
+    ],
+    exports: [
+        SearchComponent,
+    ]
+})
+export class SearchModule {
+
+}
